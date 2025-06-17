5382a24cee3fda671234089d677d729dfc868fae
FARHAN MOHAMMAD <mxwin222@gmail.com>
2025-06-17 08:19:08 +0530

    Initial commit

---
+++ src/content_option.js
@@ -0,0 +1,167 @@
+const logotext = "MF";
+const meta = {
+    title: "MF",
+    description: "I’m Farhan Mohammad data scientist _ Full stack devloper,currently working in Udaipur",
+};
+
+const introdata = {
+    title: "Hello I’m Farhan",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "Hi, I'm FARHAN, a passionate web developer with expertise in creating dynamic and user-friendly websites. With a strong foundation in HTML, CSS, and JavaScript, I excel in both front-end and back-end development. My experience includes working with popular frameworks like React and Vue.js, as well as server-side technologies such as Node.js and Python. I thrive on turning complex problems into simple, intuitive solutions and am always eager to stay current with emerging technologies and best practices in web development.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "Hello, I'm FARHAN, a passionate Web Developer with expertise in creating dynamic and user-friendly websites. With a strong foundation in HTML, CSS, and JavaScript, I excel in both front-end and back-end development. My experience includes working with popular frameworks like React and Vue.js, as well as server-side technologies such as Node.js and Python. I thrive on turning complex problems into simple, intuitive solutions and am always eager to stay current with emerging technologies and best practices in web development.",
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "YAdfi",
+        date: "2020",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Jamalya",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "ALquds",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Djano",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "UI/UX development focuses on designing and optimizing digital interfaces to ensure an intuitive, engaging, and seamless user experience. ",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Mobile app development involves designing, building, and deploying software applications for smartphones and tablets. It includes native (iOS/Android), hybrid, or cross-platform development approaches to deliver high-performance, user-friendly, and scalable mobile solutions.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "WordPress development involves designing, building, and maintaining websites using the WordPress CMS (Content Management System). It includes custom theme development, plugin integration, performance optimization, and security enhancements to create scalable, user-friendly, and SEO-optimized websites.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "MXWIN222@GMAIL.COM",
+    YOUR_FONE: "(890)530-390-8",
+    description: "Feel free to connect with me to discuss how I can contribute to your next project or collaborate on innovative web solutions. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_8xw60m2",
+    YOUR_TEMPLATE_ID: "template_e15dugf",
+    YOUR_USER_ID: "_rLPTCAv8iilAqfAo",
+    YOUR_PUBLIC_KEY: '_rLPTCAv8iilAqfAo',// Your actual Public Key
+};  
+
+const socialprofils = {
+    github: "https://github.com/farhan890-tech",
+    facebook: "https://facebook.com",
+    linkedin: "https://www.linkedin.com/in/mohammad-farhan-?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    twitter: "https://x.com/FarhanMoha11632",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
