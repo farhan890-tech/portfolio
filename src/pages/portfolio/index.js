2fc5790b78be5cb51ed4561504660636557500b4
Farhan Mohammad <162948576+farhan890-tech@users.noreply.github.com>
2025-07-19 12:02:54 +0530

    Update index.js

--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -26,7 +26,7 @@ export const Portfolio = () => {
                 <img src={data.img} alt="" />
                 <div className="content">
                   <p>{data.description}</p>
-                  <a href={data.link}>view project</a>
+                  <a href={data.https://github.com/farhan890-tech/-Employ-Directory-Application}>view project</a>
                 </div>
               </div>
             );
